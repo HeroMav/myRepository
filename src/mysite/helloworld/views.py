e8fab3ec6bc2517c657c48a486c4a1c54fa42292
Yeoh Keng Wei <keng_wei2000@yahoo.com>
2013-04-20 17:18:01 +0800

    Written helloworld app.

--- src/mysite/helloworld/views.py
+++ src/mysite/helloworld/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from django.shortcuts import render
+from helloworld.models import ClickMe
+
+def user_view(request):
+    return render(request, "index.html")
+
+def click_button(request):
+    clickMe = ClickMe()
+    clickMe.save()
+    return render(request, "index.html")
--- src/mysite/mysite/urls.py
+++ src/mysite/mysite/urls.py
@@ -14,4 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'helloworld.views.user_view'),
+    url(r'^index/$', 'helloworld.views.user_view'),
+    url(r'^button/$', 'helloworld.views.click_button'),
 )
